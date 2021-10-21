78f43112ac46cc5cf740ad2f56420f8fd9f514df
Michael Loucks <michael.loucks@codeimmersives.com>
2021-10-20 22:25:34 -0500

    trying to fix error

--- src/components/section/Header.js
+++ src/components/section/Header.js
@@ -31,8 +31,7 @@ const Header = () => {
         <div>
             <div className="Header">
                 <h1>{ `I'm ${header.name}` }</h1>
-            <p className="line-1 anim-typewriter">and this is my portfolio...
-            /p>
+            <p className="line-1 anim-typewriter">and this is my portfolio... </p>
             <label className="switch">
                 <input id="mode-switch" onClick={e => toggleDarkMode(e)} type="checkbox"/>
                 <span className="slider round"></span>
