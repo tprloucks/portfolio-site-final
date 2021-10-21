95bc9c40c6b5be638d323f0e306b09a88a5b6034
Michael Loucks <michael.loucks@codeimmersives.com>
2021-10-20 22:23:28 -0500

    did not work

--- src/components/section/Header.js
+++ src/components/section/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
         <div>
             <div className="Header">
                 <h1>{ `I'm ${header.name}` }</h1>
-            <p className="line-1 anim-typewriter">and this is my portfolio...Full Stack Developer/Technical Recruiter
+            <p className="line-1 anim-typewriter">and this is my portfolio...
             /p>
             <label className="switch">
                 <input id="mode-switch" onClick={e => toggleDarkMode(e)} type="checkbox"/>
